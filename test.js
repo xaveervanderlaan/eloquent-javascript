ef9e7b8be4416ea28e521725772f4aad671fe01d
Xaveer van der Laan <xavlaan@protonmail.com>
2024-09-28 00:31:31 +0200

    Test stuff (sleepy)

--- test.js
+++ test.js
@@ -8,5 +8,36 @@ const arr = [1, 2, 3];
 obj.test = 'qwe';
 obj.test = {test: {q: null}};
 
+obj.test2 = Object.assign({}, obj.test);
+
 console.log(obj);
 console.log(arr);
+
+console.log('-------------------');
+
+let data = {};
+
+for (let index = 0; index < arr.length; index++) {
+
+    const element = arr[index];
+    const struc = { value: null, rest: null };
+
+
+    let temp_obj;
+
+    temp_obj = Object.assign({}, { value: element, rest: null });
+
+    data = Object.assign(temp_obj);
+
+    console.log(temp_obj);
+    if (arr[index + 1]) {
+
+        data = Object.assign(temp_obj);
+
+    }
+
+    
+
+}
+
+console.log(data);
